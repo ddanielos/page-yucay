930c08213057494af50e67c06d347f80abee2eea
ddanielos <dany.oblitas1984@gmail.com>
2020-11-04 19:16:51 -0500

    menu-responsive-hover con hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import '../assets/styles/styles.styl';
 import background from '../assets/static/img/background_hero.png';
 import title from '../assets/static/img/hero.png';
+import logo from '../assets/static/img/logo.png';
+import '../assets/styles/styles.styl';
+
 
 const Hero = () => {
    return(
       <>
          <div className="hero">
-            <img className="hero__background" src={ background } alt="Fabrica Yucay Brewery"/>
-            <div className="hero__background-device" style={{backgroundImage:`url(${background})`}}></div>
-            <img className="hero__title" src={ title } alt="Cerveceria artesanal"/>
+            {/* <img className="hero__background" src={ background } alt="Fabrica Yucay Brewery"/> */}
+            <div className="hero__background" style={{backgroundImage:`url(${background})`}}>
+               <img className="hero__title" src={ title } alt="Cerveceria artesanal"/>
+            </div>
+            <div className="hero__background-device" style={{backgroundImage:`url(${background})`}}>
+               <img className="hero__logo" src={ logo } alt="Cerveceria artesanal"/>
+            </div>
+
          </div>
       </>
    )
