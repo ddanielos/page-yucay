adaa6e8e89ec17a4053d691cafee2bd3a376fdd0
dany-dev <dany.oblitas1984@gmail.com>
2022-04-26 00:17:08 -0500

    router-in-proc

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Blog from '../containers/Blog';
 import Home from '../containers/Home';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
-   <Home />
+   <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home/>} />
+         <Route path="blog" element={<Blog/>} />
+         <Route path="*" element={<NotFound/>} />
+      </Routes>
+   </BrowserRouter>
 );
 
 export default App;
